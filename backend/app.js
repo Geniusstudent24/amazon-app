81d539fb9d300c7b34c92799255aebe1595cd170
Meet Nimavat <meetnimavat725@gmail.com>
2026-06-18 11:50:24 +0530

    Fix CORS origin URL in app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-  origin: "https://amazon-app-meet.vercel.app/",
+  origin: "https://amazon-app-meet.vercel.app.com",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
