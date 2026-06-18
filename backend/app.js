3181b02d5621fba7a18fd0508e6341e5ff5d46fd
Meet Nimavat <meetnimavat725@gmail.com>
2026-06-18 07:38:28 +0530

    Update CORS origin to production URL

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://amazon-app-meet.vercel.app/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
